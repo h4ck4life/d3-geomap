0224787fa73485d31a1d2dd6361df54a7db270c1
Alif <mohd.alif@dhl.com>
2021-01-20 14:27:42 +0800

    Working version of label sizing on zoomed maps

--- maps.js
+++ maps.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 
 let width = 900;
 let height = 600;
-//let zoomCountries = ['CN', 'IN', 'RU'];
-let zoomCountries = ['MY', 'TH', 'PH', 'CN'];
+let zoomCountries = ['CN', 'IN', 'TH'];
+//let zoomCountries = ['MQ'];
 //let zoomCountries = [];
 
 let projection = d3.geoMercator()
@@ -12,13 +13,12 @@ let projection = d3.geoMercator()
 let svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
+let g = svg.append("g");
 
 let path = d3.geoPath()
     .projection(projection);
 
-let g = svg.append("g");
-
-let getZoomCountriesMap = function () {
+const getZoomCountriesMap = function () {
     let countries = [];
     svg.selectAll("path").each(function (d) {
         if (zoomCountries.includes(d.properties.iso_3166_1_alpha_2_codes)) {
@@ -28,7 +28,12 @@ let getZoomCountriesMap = function () {
     return countries;
 }
 
-let colors = ['white', 'blue', 'red'];
+const getElementCoords = function (coords) {
+    var ctm = coords.getCTM(),
+        x = (ctm.e + coords.getBBox().x * ctm.a + coords.getBBox().y * ctm.c) + coords.getBoundingClientRect().width / 2,
+        y = (ctm.f + coords.getBBox().x * ctm.b + coords.getBBox().y * ctm.d) + coords.getBoundingClientRect().height / 2;
+    return { x: x, y: y };
+};
 
 d3.json("world.topo.json")
     .then((world) => {
@@ -38,18 +43,19 @@ d3.json("world.topo.json")
             .enter()
             .append("path")
             .attr("d", path)
+            .attr("id", d => d.properties.iso_3166_1_alpha_2_codes)
             .style('fill', (d) => {
                 if (zoomCountries.includes(d.properties.iso_3166_1_alpha_2_codes)) {
-                    //return colors[Math.floor(Math.random() * 2) + 1];
-                    return '#' + Math.floor(Math.random()*16777215).toString(16);
+                    return '#' + Math.floor(Math.random() * 16777215).toString(16);
                 }
-            });
+            })
+            .append('title')
+            .text(d => `${d.properties.name} - ${d.properties.iso_3166_1_alpha_2_codes}`);
     })
     .then(() => {
         return getZoomCountriesMap().map((d) => path.bounds(d));
     })
     .then((allBounds) => {
-        console.log(allBounds);
         if (allBounds.length > 0) {
             var bound0 = d3.min(allBounds, function (d) {
                 return d[0][0]
@@ -69,51 +75,49 @@ d3.json("world.topo.json")
             let dy = bound3 - bound1;
             let x = (bound0 + bound2) / 2;
             let y = (bound1 + bound3) / 2;
-            /* scale = .9 / Math.max(dx / width, dy / height);
-            translate = [width / 2 - scale * x, height / 2 - scale * y]; */
-
             let scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height)));
+            //let scale = .9 / Math.max(dx / width, dy / height);
             let translate = [width / 2 - scale * x, height / 2 - scale * y];
 
+            console.log(`scale --> ${scale} | translate --> ${translate}`);
+
             // Render the zoomed map
-            svg.select("g").attr("transform", "translate(" + translate + ")scale(" + scale + ")")
+            svg.select("g").attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+
+            return svg.select("g");
         }
     })
-    .then(() => {
-        /* getZoomCountriesMap().forEach(data => {
-            
-            console.log(data);
+    .then((mapGroupElement) => {
 
-            let countryLabel = g.append("g");
-            
-            countryLabel.append('rect')
-                .attr("x", function (d) {
-                    return path.centroid(data)[0];
-                })
-                .attr("y", function (d) {
-                    return path.centroid(data)[1];
-                })
-                .attr("width", 40)
-                .attr("height", 15)
-                .style("fill", "white")
-                .style("stroke", "yellow")
-                .style("stroke-width", "0.5px")
-                .style("fill-opacity", "0.5");
+        let labelGroup = svg.data(getZoomCountriesMap()).append("g");
 
-            countryLabel.append('text')
-                .attr("x", function (d) {
-                    return path.centroid(data)[0] + 10;
-                })
-                .attr("y", function (d) {
-                    return path.centroid(data)[1] + 10;
-                })
-                .text(function (d) {
-                    return data.properties.name;
-                })
-                .style("fill", "yellow")
-                .style("font-size", "12px");
+        labelGroup.append('rect')
+            .attr("x", function (d) {
+                return getElementCoords(d3.select("path#" + d.properties.iso_3166_1_alpha_2_codes).node()).x;
+            })
+            .attr("y", function (d) {
+                return getElementCoords(d3.select("path#" + d.properties.iso_3166_1_alpha_2_codes).node()).y;
+            })
+            .attr("width", 40)
+            .attr("height", 15)
+            .style("fill", "white")
+            .style("stroke", "white")
+            .style("stroke-width", "0.5px")
+            .style("fill-opacity", "0.8")
 
-        }); */
+        labelGroup.append('text')
+            .attr("x", function (d) {
+                return getElementCoords(d3.select("path#" + d.properties.iso_3166_1_alpha_2_codes).node()).x + 10;
+            })
+            .attr("y", function (d) {
+                return getElementCoords(d3.select("path#" + d.properties.iso_3166_1_alpha_2_codes).node()).y + 12;
+            })
+            .text(function (d) {
+                return d.properties.name;
+            })
+            .style("fill", "black")
+            .style('font-weight', 'bold')
+            .style("font-size", "12px");
     })
     .catch((error) => {
         console.error(error);
